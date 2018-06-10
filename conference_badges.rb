57e0131547efde7984a56772bd030be9bd1876f6
Catherine Bronzo <cbronzo@mac.com>
2018-06-10 20:50:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(array)
-  array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   array.each do |speaker|
    puts "Hello, my name is #{name}."
  end
